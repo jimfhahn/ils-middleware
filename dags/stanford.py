38c83cd88f83d6485fc76566b83a676a711f60c8
Aaron Collier <aaron.collier@stanford.edu>
2021-09-22 14:27:10 -0700

    Use an AwsLambdaHook to trigger the rdf2marc lambda

--- dags/stanford.py
+++ dags/stanford.py
@@ -13,13 +13,44 @@ from folio_login import FolioLogin
 
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+<<<<<<< HEAD
 from airflow.operators.python import PythonOperator
+=======
+from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.contrib.hooks.aws_lambda_hook import AwsLambdaHook
+
+
+def choose_ils(**kwargs) -> str:
+    import random
+
+    ils_choice = random.random()
+    logging.info(f"Random ILS Choice {ils_choice}")
+    if ils_choice <= 0.50:
+        return "symphony_json"
+    return "symphony_json"  # return "folio_map"
+>>>>>>> fabab16 (Use an AwsLambdaHook to trigger the rdf2marc lambda)
 
 
 def retrive_rdf2marc(**kwargs) -> str:
     """Stub function for processing MARC file from running rdf2marc."""
     logging.info(f"Retrieves MARC file from rdf2marc {kwargs}")
-    return "update_sinopia"
+    lambda_hook = AwsLambdaHook(
+        'sinopia-rdf2marc-development',
+        log_type='None',
+        qualifier='$LATEST',
+        invocation_type='RequestResponse',
+        config=None,
+        aws_conn_id='aws_lambda_connection'
+    )
+    logging.info(f"Hook = {lambda_hook}")
+    payload = """
+    {"instance_uri": "https://api.stage.sinopia.io/resource/3eb5f480-60d6-4732-8daf-02d8d6f26eb7",
+     "bucket": "sinopia-marc-development",
+     "marc_path": "marc/airflow/3eb5f480-60d6-4732-8daf-02d8d6f26eb7/record.mar",
+     "marc_txt_path": "marc/airflow/3eb5f480-60d6-4732-8daf-02d8d6f26eb7/record.txt",
+     "error_path": "marc/airflow/3eb5f480-60d6-4732-8daf-02d8d6f26eb7/error.txt"
+    }"""
+    return lambda_hook.invoke_lambda(payload=payload)
 
 
 default_args = {
@@ -45,9 +76,13 @@ with DAG(
     messages = SubscribeOperator(queue="stanford-ils")
 
     # Retrive MARC from Sinopia API, convert to MARC JSON,
-    setup_rdf2marc = GitRdf2Marc()
+    # setup_rdf2marc = GitRdf2Marc()
 
+<<<<<<< HEAD
     run_rdf2marc = Rdf2Marc(instance_url="")
+=======
+    # run_rdf2marc = Rdf2Marc(instance_url=urls[0])
+>>>>>>> fabab16 (Use an AwsLambdaHook to trigger the rdf2marc lambda)
 
     sinopia_to_symphony_json = PythonOperator(
         task_id="symphony_json",
@@ -69,9 +104,22 @@ with DAG(
         task_id="processed_sinopia", dag=dag, trigger_rule="none_failed"
     )
     # Updates Sinopia URLS with HRID
+<<<<<<< HEAD
     update_sinopia = UpdateIdentifier(urls=[])
 
 messages >> [setup_rdf2marc, sinopia_to_folio_records]
 (setup_rdf2marc >> run_rdf2marc >> sinopia_to_symphony_json >> processed_sinopia)
+=======
+    update_sinopia = UpdateIdentifier(urls=urls)
+
+listen_sns >> branch_ils >> [sinopia_to_symphony_json, sinopia_to_folio_records]
+(
+    # setup_rdf2marc
+    # >> run_rdf2marc
+    sinopia_to_symphony_json
+    >> send_to_symphony
+    >> processed_sinopia
+)
+>>>>>>> fabab16 (Use an AwsLambdaHook to trigger the rdf2marc lambda)
 sinopia_to_folio_records >> send_to_folio >> processed_sinopia
 processed_sinopia >> update_sinopia
