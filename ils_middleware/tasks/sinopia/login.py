eb383bdbc09f787c8cf7d54dd3d21b2b3f96fd59
Jeremy Nelson <jermnelson@gmail.com>
2021-10-13 15:12:07 -0600

    New Login function

---
+++ ils_middleware/tasks/sinopia/login.py
@@ -0,0 +1,22 @@
+"""Sinopia Login to generate a Cognito JWT."""
+import boto3
+from airflow.models import Variable
+
+
+def sinopia_login(**kwargs):
+    """Log into Sinopia using Airflow Variables."""
+    region = kwargs.get("aws_region", "us-west-1")
+    sinopia_env = kwargs.get("sinopia_env", "dev")
+    sinopia_user = Variable.get("sinopia_user")
+    sinopia_password = Variable.get("sinopia_password")
+    cognito_app_client_id = Variable.get(f"{sinopia_env}_cognito_client_id")
+
+    client = kwargs.get('client', boto3.client("cognito-idp", region))
+
+    login_response = client.initiate_auth(
+        AuthFlow="USER_PASSWORD_AUTH",
+        AuthParameters={"USERNAME": sinopia_user, "PASSWORD": sinopia_password},
+        ClientId=cognito_app_client_id,
+    )
+
+    return login_response.get("AuthenticationResult").get("AccessToken")
---
+++ tests/tasks/sinopia/test_sinopia_login.py
@@ -0,0 +1,50 @@
+"""Tests Sinopia Login Function."""
+
+import pytest
+import boto3
+
+import botocore.session
+from botocore.stub import Stubber
+
+
+from airflow.models import Variable
+from pytest_mock import MockerFixture
+
+from ils_middleware.tasks.sinopia.login import sinopia_login
+
+
+@pytest.fixture
+def mock_botocore_client(monkeypatch):
+
+    def mock_initiate_auth(*args, **kwargs):
+        return { "AuthenticationResult": { "AccessToken": "abc12345"} }
+
+    monkeypatch.setattr(botocore.client.CognitoIdentityProvider, "initiate_auth", mock_initiate_auth)
+
+@pytest.fixture
+def mock_variable(monkeypatch):
+    def mock_get(key, default=None):
+        if key == "sinopia_user":
+            return "ils_middleware"
+
+        if key == "sinopia_password":
+            return "a_pwd_1234"
+
+        if key == "dev_cognito_client_id":
+            return "abcd3445efg"
+
+    monkeypatch.setattr(Variable, "get", mock_get)
+
+
+
+def test_sinopia_login(mock_variable):
+    cognito_idp = botocore.session.get_session().create_client('cognito-idp', 'us-west-1')
+    stubber = Stubber(cognito_idp)
+
+    response = { "AuthenticationResult": { 'AccessToken': 'abc12345'}}
+
+    stubber.add_response('initiate_auth', response)
+    stubber.activate()
+
+    jwt = sinopia_login(client=cognito_idp)
+    assert jwt == "abc12345"
