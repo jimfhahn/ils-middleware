bfb422666334e1a7c600ab541fa464831fb3f4fb
Jeremy Nelson <jermnelson@gmail.com>
2021-10-14 20:16:48 -0600

    Retrieves instance from XCOM, converts string to JSON when needed

--- ils_middleware/tasks/amazon/s3.py
+++ ils_middleware/tasks/amazon/s3.py
@@ -4,23 +4,25 @@ import os
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from pymarc import MARCReader
 
+logger = logging.getLogger(__name__)
 
 def get_from_s3(**kwargs) -> dict:
-    task_instance = kwargs["task_instance"]
-    instance_id = task_instance.xcom_pull(task_ids="symphony_json")
+    instance_id = kwargs.get("instance_id")
     s3_hook = S3Hook(aws_conn_id="aws_lambda_connection")
-
+    
     temp_file = s3_hook.download_file(
         key=f"marc/airflow/{instance_id}/record.mar",
         bucket_name="sinopia-marc-development",
     )
 
-    return {"id": instance_id, "temp_file": temp_file}
+    return json.dumps({"id": instance_id, "temp_file": temp_file})
 
 
-def send_to_s3(**kwargs) -> str:
+def send_to_s3(**kwargs) -> dict:
     s3_hook = S3Hook(aws_conn_id="aws_lambda_connection")
-    instance = get_temp_instances(kwargs["task_instance"])
+    instance = kwargs.get("instance")
+    if isinstance(instance, str):
+        instance = json.loads(instance)
     marc_record = marc_record_from_temp_file(instance)
 
     s3_hook.load_string(
@@ -30,12 +32,8 @@ def send_to_s3(**kwargs) -> str:
         replace=True,
     )
 
-    return json.dumps(marc_record.as_json())
+    return marc_record.as_json()
 
-
-def get_temp_instances(task_instance):
-    """Returns the temp file location from the prior task"""
-    return task_instance.xcom_pull(task_ids="process_symphony.download_marc")
 
 
 def marc_record_from_temp_file(instance):
@@ -45,5 +43,5 @@ def marc_record_from_temp_file(instance):
         with open(temp_file, "rb") as marc:
             return next(MARCReader(marc))
     else:
-        logging.error(f"MARC data for {instance_id} missing or empty.")
+        logger.error(f"MARC data for {instance_id} missing or empty.")
         raise Exception()
