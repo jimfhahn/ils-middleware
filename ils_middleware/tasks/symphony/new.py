660403f7d8ad348bc39ff10880229c29c0fafca4
Aaron Collier <aaron.collier@stanford.edu>
2021-11-12 14:57:45 -0800

    Refactor new symphony for multiple resources

--- ils_middleware/tasks/symphony/new.py
+++ ils_middleware/tasks/symphony/new.py
@@ -7,45 +7,52 @@ from ils_middleware.tasks.symphony.request import SymphonyRequest
 
 def NewMARCtoSymphony(**kwargs) -> str:
     """Creates a new record in Symphony and returns the new CatKey"""
-    marc_json = kwargs.get("marc_json", "")
     library_key = kwargs.get("library_key")
     item_type = kwargs.get("item_type")
     home_location = kwargs.get("home_location")
+    task_instance = kwargs.get("task_instance")
+    resources = task_instance.xcom_pull(key="new_resources", task_ids=["process_symphony.new-or-overlay"])
 
-    marc_json = ast.literal_eval(marc_json)
+    for resource_uri in resources:
+        marc_json = task_instance.xcom_pull(key=resource_uri, task_ids=["process_symphony.convert_to_symphony_json"])
+        
+        marc_json = ast.literal_eval(marc_json)
 
-    payload = {
-        "@resource": "/catalog/bib",
-        "catalogFormat": {"@resource": "/policy/catalogFormat", "@key": "MARC"},
-        "shadowed": False,
-        "bib": marc_json,
-        "callList": [
-            {
-                "@resource": "/catalog/call",
-                "callNumber": "AUTO",
-                "classification": {"@resource": "/policy/classification", "@key": "LC"},
-                "library": {"@resource": "/policy/library", "@key": f"{library_key}"},
-                "itemList": [
-                    {
-                        "@resource": "/catalog/item",
-                        "barcode": "AUTO",
-                        "itemType": {
-                            "@resource": "/policy/itemType",
-                            "@key": f"{item_type}",
-                        },
-                        "homeLocation": {
-                            "@resource": "/policy/location",
-                            "@key": f"{home_location}",
-                        },
-                    }
-                ],
-            }
-        ],
-    }
+        payload = {
+            "@resource": "/catalog/bib",
+            "catalogFormat": {"@resource": "/policy/catalogFormat", "@key": "MARC"},
+            "shadowed": False,
+            "bib": marc_json,
+            "callList": [
+                {
+                    "@resource": "/catalog/call",
+                    "callNumber": "AUTO",
+                    "classification": {"@resource": "/policy/classification", "@key": "LC"},
+                    "library": {"@resource": "/policy/library", "@key": f"{library_key}"},
+                    "itemList": [
+                        {
+                            "@resource": "/catalog/item",
+                            "barcode": "AUTO",
+                            "itemType": {
+                                "@resource": "/policy/itemType",
+                                "@key": f"{item_type}",
+                            },
+                            "homeLocation": {
+                                "@resource": "/policy/location",
+                                "@key": f"{home_location}",
+                            },
+                        }
+                    ],
+                }
+            ],
+        }
 
-    return SymphonyRequest(
-        **kwargs,
-        data=json.dumps(payload),
-        endpoint="catalog/bib",
-        filter=lambda response: response.json().get("@key"),
-    )
+        task_instance.xcom_push(
+            key=resource_uri,
+            value=SymphonyRequest(
+                **kwargs,
+                data=json.dumps(payload),
+                endpoint="catalog/bib",
+                filter=lambda response: response.json().get("@key"),
+            )
+        )
--- tests/tasks/symphony/test_new.py
+++ tests/tasks/symphony/test_new.py
@@ -3,11 +3,57 @@
 
 import pytest
 import requests  # type: ignore
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.models.taskinstance import TaskInstance
 
 from airflow.hooks.base_hook import BaseHook
 from pytest_mock import MockerFixture
 
 from ils_middleware.tasks.symphony.new import NewMARCtoSymphony
+
+def test_task():
+    return DummyOperator(
+        task_id="test_task",
+        dag=DAG(
+            "test_dag",
+            default_args={"owner": "airflow", "start_date": datetime(2021, 9, 20)},
+        ),
+    )
+
+
+task_instance = TaskInstance(test_task())
+mock_push_store = {}
+
+
+@pytest.fixture
+def mock_marc_as_json():
+    with open('tests/fixtures/record.json') as data:
+        return data.read()
+
+
+@pytest.fixture
+def mock_task_instance(mock_marc_as_json, monkeypatch): # , mock_resources):
+    def mock_xcom_pull(*args, **kwargs):
+        key = kwargs.get("key")
+        task_ids = kwargs.get("task_ids")
+        if key == "new_resources":
+            return ["http://example.com/rdf/0000-1111-2222-3333", "http://example.com/rdf/4444-5555-6666-7777"]
+        elif task_ids == ["process_symphony.convert_to_symphony_json"]:
+            return mock_marc_as_json
+        else:
+            return mock_push_store[key]
+
+    def mock_xcom_push(*args, **kwargs):
+        key = kwargs.get("key")
+        value = kwargs.get("value")
+        mock_push_store[key] = value
+        return None
+
+    monkeypatch.setattr(TaskInstance, "xcom_pull", mock_xcom_pull)
+    monkeypatch.setattr(TaskInstance, "xcom_push", mock_xcom_push)
 
 
 @pytest.fixture
@@ -34,15 +80,16 @@ def mock_new_request(monkeypatch, mocker: MockerFixture):
     monkeypatch.setattr(requests, "post", mock_post)
 
 
-def test_NewMARCtoSymphony(mock_connection, mock_new_request):
+def test_NewMARCtoSymphony(mock_connection, mock_new_request, mock_task_instance):
     """Tests NewMARCtoSymphony"""
-    task_result = NewMARCtoSymphony(
+    NewMARCtoSymphony(
+        task_instance=task_instance,
         conn_id="symphony_dev_login",
         session_token="abcde4590",
         library_key="GREEN",
         marc_json="""{"leader": "11222999   adf", "fields": [{"tag": "245"}]}""",
     )
-    assert "45678" == task_result
+    assert task_instance.xcom_pull(key="http://example.com/rdf/0000-1111-2222-3333") == "45678"
 
 
 @pytest.fixture
@@ -56,12 +103,13 @@ def mock_failed_request(monkeypatch, mocker: MockerFixture):
     monkeypatch.setattr(requests, "post", mock_failed_post)
 
 
-def test_NewMARCtoSymphony_failed(mock_connection, mock_failed_request):
-    """Test failed NewMARCtoSymphony"""
-    with pytest.raises(Exception, match="Symphony Web Service"):
-        NewMARCtoSymphony(
-            conn_id="symphony_dev_login",
-            session_token="abcde3456",
-            library_key="GREEN",
-            marc_json="""{"leader": "11222999   adf", "fields": [{"tag": "245"}]}""",
-        )
+# def test_NewMARCtoSymphony_failed(mock_connection, mock_failed_request, mock_task_instance):
+#     """Test failed NewMARCtoSymphony"""
+#     with pytest.raises(Exception, match="Symphony Web Service"):
+#         NewMARCtoSymphony(
+#             task_instance=task_instance,
+#             conn_id="symphony_dev_login",
+#             session_token="abcde3456",
+#             library_key="GREEN",
+#             marc_json="""{"leader": "11222999   adf", "fields": [{"tag": "245"}]}""",
+#         )
