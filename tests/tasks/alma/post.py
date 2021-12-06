1af7de1e3d82e40f63385ee57fb506db1f6a37e5
jim hahn <jimfhahn@gmail.com>
2021-12-06 07:10:12 -0600

    Update post.py

--- tests/tasks/alma/post.py
+++ tests/tasks/alma/post.py
@@ -1,2 +1,2 @@
-"""Write test for new.py use examples from /tests/tasks/symphony/test_symphony_request.py
+"""Write test for post.py use examples from /tests/tasks/symphony/test_symphony_request.py
 """
