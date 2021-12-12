c276e6f96ac73344a88fc128f3bffa362639ea6d
jim hahn <jimfhahn@gmail.com>
2021-12-12 12:50:27 -0600

    removed unused imports.

--- tests/tasks/amazon/test_alma_s3.py
+++ tests/tasks/amazon/test_alma_s3.py
@@ -1,12 +1,7 @@
 """Test the Alma AWS S3 tasks properly name and load files."""
 import pytest
-from unittest import mock
 
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
-from ils_middleware.tasks.amazon.alma_s3 import get_from_alma_s3
-
-from tasks import test_task_instance
 
 
 @pytest.fixture
