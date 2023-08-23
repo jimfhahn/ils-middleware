8004e0ba4435f39dbe104deecbbaeca8443f8f82
jim hahn <jimfhahn@gmail.com>
2023-08-23 16:01:24 -0500

    simplify POST/PUT

--- ils_middleware/tasks/alma/post_bfinstance.py
+++ ils_middleware/tasks/alma/post_bfinstance.py
@@ -11,6 +11,16 @@ import lxml.etree as ET
 logger = logging.getLogger(__name__)
 
 
+def parse_400(result):
+    xml_response = ET.fromstring(result)
+    xslt = ET.parse("ils_middleware/tasks/alma/xslt/put_mms_id.xsl")
+    transform = ET.XSLT(xslt)
+    result_tree = transform(xml_response)
+    put_mms_id_str = str(result_tree)
+    logger.debug(f"put_mms_id_str: {put_mms_id_str}")
+    return put_mms_id_str
+
+
 def NewInstancetoAlma(**kwargs):
     s3_hook = S3Hook(aws_conn_id="aws_lambda_connection")
     task_instance = kwargs.get("task_instance")
@@ -24,70 +34,84 @@ def NewInstancetoAlma(**kwargs):
         key=f"/alma/{instance_id}/bfinstance_alma.xml",
         bucket_name=Variable.get("marc_s3_bucket"),
     )
-
     task_instance.xcom_pull(key=instance_uri, task_ids=temp_file)
     with open(temp_file, "rb") as f:
         data = f.read()
-    logger.debug(f"file data: {data}")
-    uri_region = Variable.get("alma_uri_region_na")
-    alma_api_key = Variable.get("alma_api_key_penn")
-
-    alma_uri = (
-        uri_region
-        + "/almaws/v1/bibs?"
-        + "from_nz_mms_id=&from_cz_mms_id=&normalization=&validate=false"
-        + "&override_warning=true&check_match=false&import_profile=&apikey="
-        + alma_api_key
-    )
-    # post to alma
-    alma_result = requests.post(
-        alma_uri,
-        headers={
-            "Content-Type": "application/xml; charset=utf-8",
-            "Accept": "application/xml",
-            "x-api-key": alma_api_key,
-        },
-        data=data,
-    )
-    logger.debug(f"alma result: {alma_result.status_code}\n{alma_result.text}")
-    result = alma_result.content
-    status = alma_result.status_code
-    xml_response = ET.fromstring(result)
-    # run xslt on the result in case the response is 400 and we need to update the record
-    xslt = ET.parse("ils_middleware/tasks/alma/xslt/put_mms_id.xsl")
-    transform = ET.XSLT(xslt)
-    result_tree = transform(xml_response)
-    put_mms_id_str = str(result_tree)
-    logger.debug(f"put_mms_id_str: {put_mms_id_str}")
-    mms_id = xml_response.xpath("//mms_id/text()")
-    logger.debug(f"mms_id: {mms_id}")
-    if status == 200:
-        task_instance.xcom_push(key=instance_uri, value=mms_id)
-    elif status == 400:
-        alma_update_uri = (
+        logger.debug(f"file data: {data}")
+        uri_region = Variable.get("alma_uri_region_na")
+        alma_api_key = Variable.get("alma_api_key_penn")
+        alma_uri = (
             uri_region
-            + "/almaws/v1/bibs/"
-            + put_mms_id_str
-            + "?normalization=&validate=false&override_warning=true"
-            + "&override_lock=true&stale_version_check=false&cataloger_level=&check_match=false"
-            + "&apikey="
+            + "/almaws/v1/bibs?"
+            + "from_nz_mms_id=&from_cz_mms_id=&normalization=&validate=false"
+            + "&override_warning=true&check_match=false&import_profile=&apikey="
             + alma_api_key
         )
-        put_update = requests.put(
-            alma_update_uri,
+        # post to alma
+        alma_result = requests.post(
+            alma_uri,
             headers={
-                "Content-Type": "application/xml; charset=UTF-8",
+                "Content-Type": "application/xml; charset=utf-8",
                 "Accept": "application/xml",
                 "x-api-key": alma_api_key,
             },
             data=data,
         )
-        logger.debug(f"put update: {put_update.status_code}\n{put_update.text}")
-        put_update_status = put_update.status_code
-        result = put_update.content
-        xml_response = ET.fromstring(result)
-        put_mms_id = xml_response.xpath("//mms_id/text()")
-        if put_update_status == 200:
-            task_instance.xcom_push(key=instance_uri, value=put_mms_id)
-        if put_update_status != 200:
-            raise Exception(f"Unexpected status code: {status}")
+        logger.debug(f"alma result: {alma_result.status_code}\n{alma_result.text}")
+        result = alma_result.content
+        status = alma_result.status_code
+        if status == 200:
+            xml_response = ET.fromstring(result)
+            mms_id = xml_response.xpath("//mms_id/text()")
+            logger.debug(f"mms_id: {mms_id}")
+            task_instance.xcom_push(key=instance_uri, value=mms_id)
+        elif status == 400:
+            # run xslt on the result in case the response is 400 and we need to update the record
+            put_mms_id_str = parse_400(result)
+            putInstanceToAlma(
+                put_mms_id_str,
+                data,
+                alma_api_key,
+                uri_region,
+                task_instance,
+                instance_uri,
+            )
+        else:
+            raise Exception(f"Unexpected status code from Alma API: {status}")
+
+
+def putInstanceToAlma(
+    put_mms_id_str,
+    data,
+    alma_api_key,
+    uri_region,
+    task_instance,
+    instance_uri,
+):
+    alma_update_uri = (
+        uri_region
+        + "/almaws/v1/bibs/"
+        + put_mms_id_str
+        + "?normalization=&validate=false&override_warning=true"
+        + "&override_lock=true&stale_version_check=false&cataloger_level=&check_match=false"
+        + "&apikey="
+        + alma_api_key
+    )
+    put_update = requests.put(
+        alma_update_uri,
+        headers={
+            "Content-Type": "application/xml; charset=UTF-8",
+            "Accept": "application/xml",
+            "x-api-key": alma_api_key,
+        },
+        data=data,
+    )
+    logger.debug(f"put update: {put_update.status_code}\n{put_update.text}")
+    put_update_status = put_update.status_code
+    result = put_update.content
+    xml_response = ET.fromstring(result)
+    put_mms_id = xml_response.xpath("//mms_id/text()")
+    if put_update_status == 200:
+        task_instance.xcom_push(key=instance_uri, value=put_mms_id)
+    else:
+        raise Exception(f"Unexpected status code: {put_update.status_code}")
